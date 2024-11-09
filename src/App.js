04df7e10db39c9d25a87ed87a35e20921b07027e
Amna Akram <amna.akram185@gmail.com>
2024-11-09 21:45:53 +0200

    initial react app setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Basic React app!</h1>
+    </div>
+  );
+}
+
+export default App;
